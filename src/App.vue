42a11477881394601c5a2df5ce32c21447469c08
swrzalek <stian.uek@gmail.com>
2021-11-28 23:50:13 +0100

    vue upgrade

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="justify-center flex bg-yellow-300 items-center h-screen">
     <div class="text-4xl">
-      Hello 👋🏼
+      Hello <span role="img" aria-label="hand">👋🏼</span>
     </div>
   </div>
 </template>
