39e3c65bf2af48b7feb4545434a712adc676cded
swrzalek <stian.uek@gmail.com>
2021-11-29 00:55:48 +0100

    not available info on larger devices

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="justify-center flex bg-yellow-300 items-center h-screen">
+  <div class="justify-center flex bg-yellow-300 items-center h-screen" v-if="isMobile">
     <div class="text-4xl">
-      Hello <span role="img" aria-label="hand">👋🏼</span>
+      This app is only available on mobile devices <span role="img" aria-label="hand">👋🏼</span>
     </div>
   </div>
+  <router-view v-else></router-view>
 </template>
 <script>
+import { onMounted, ref } from 'vue';
+
 export default {
   name: 'App',
+  setup() {
+    const isMobile = ref(false);
+
+    const handleResize = (e) => {
+      isMobile.value = e.target.innerWidth >= 480;
+    };
+
+    onMounted(() => {
+      window.addEventListener('resize', handleResize);
+    });
+
+    return { isMobile };
+  },
 };
 </script>
