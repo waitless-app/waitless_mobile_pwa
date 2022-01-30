5bbd69c61ed32512f34859b5504c2a10ce7120da
Sebastian Wrzalek <s.wrzalek@gmail.com>
2022-01-30 21:40:51 +0100

    Show address on premises card:

--- src/components/organisms/PremisesCard.vue
+++ src/components/organisms/PremisesCard.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="bg-green-400 w-100 rounded-md flex-col shadow-md">
     <div class="max-w-sm">
-      <div class="w-100 h-36 content-image" :style="{ backgroundImage: bg }" />
+      <div
+        class="w-100 h-36 content-image"
+        :style="{ backgroundImage: bg, filter: props.premises.active ? '' : 'grayscale(1)' }"
+      />
       <div class="p-3 bg-green-400 rounded-b-md">
         <a href="#">
-          <h5 class="text-gray-900 font-bold text-md tracking-tight mb-2">{{ props.name }}</h5>
+          <h5 class="text-gray-900 font-bold text-md tracking-tight mb-2">
+            {{ props.premises.name }}
+          </h5>
         </a>
-        <p class="font-normal text-gray-700 mb-3">Open now.</p>
+        <p class="font-normal text-gray-700 mb-3">
+          {{ formattedAddress }}
+        </p>
+        <p class="font-bold text-gray-700 text-xs">
+          {{ props.premises.active ? "Open Now" : "Closed" }}
+        </p>
       </div>
     </div>
   </div>
@@ -16,15 +26,25 @@
 import { defineProps } from "vue";
 
 const props = defineProps({
-  name: {
-    type: String,
-    default: "",
-  },
-  image: {
-    type: String,
-    default: "https://flowbite.com/docs/images/blog/image-1.jpg",
+  premises: {
+    type: Object,
+    default: () => ({
+      active: true,
+      address: "",
+      city: "",
+      country: "",
+      description: "",
+      id: 0,
+      image: "",
+      name: "",
+      owner: "",
+      postcode: "",
+      tags: [],
+    }),
   },
 });
 
-const bg = `url(${props.image})`;
+const formattedAddress = `${props.premises.country}, ${props.premises.city}, ${props.premises.address}`;
+
+const bg = `url(${props.premises.image})`;
 </script>
