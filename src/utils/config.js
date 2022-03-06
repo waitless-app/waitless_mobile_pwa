09823a58c4fd20fd8e036fe1f1c4f7290a2b53ff
Sebastian Wrzalek <s.wrzalek@gmail.com>
2022-03-06 18:21:09 +0100

    feat(core) move API url to env

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
 export const API_URL = process.env.VUE_APP_API_URL || "http://localhost:8000/api/";
-export const WS_URL = process.env.VUE_WS_API_URL || `ws://localhost:8000/notification/`;
+export const WS_URL = process.env.VUE_APP_WS_URL || `ws://localhost:8000/notification/`;
