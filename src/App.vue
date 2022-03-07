e21101f47572e8589b4926a98b56a042dac3ba44
Sebastian Wrzalek <s.wrzalek@gmail.com>
2022-03-07 14:59:19 +0100

    fix(core) remove desktop prevention screen

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
-  <div class="justify-center flex bg-yellow-300 items-center h-screen" v-if="isMobile">
-    <div class="text-4xl">
-      This app is only available on mobile devices <span role="img" aria-label="hand">👋🏼</span>
-    </div>
-  </div>
-  <AppTemplate v-else>
+  <AppTemplate>
     <SafeArea>
       <router-view v-slot="{ Component }">
         <suspense timeout="0">
@@ -21,22 +16,12 @@
   </AppTemplate>
 </template>
 <script setup>
-import { onMounted, ref, toRefs } from "vue";
+import { toRefs } from "vue";
 import { useRoute } from "vue-router";
 import AppNavBottom from "./components/organisms/AppNavigationBottom.vue";
 import SafeArea from "./components/templates/SafeArea.vue";
 import AppTemplate from "./components/templates/AppTemplate.vue";
 
-const isMobile = ref(false);
-
 const router = useRoute();
 const { meta } = toRefs(router);
-
-const handleResize = (e) => {
-  isMobile.value = e.target.innerWidth >= 480;
-};
-
-onMounted(() => {
-  window.addEventListener("resize", handleResize);
-});
 </script>
